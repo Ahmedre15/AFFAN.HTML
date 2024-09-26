c83e6eb80794ff21595686aedb9f1d07d0f97499
Ahmedre15 <ahmedaffanre15@gmail.com>
2024-09-26 19:04:00 +0500

    Add files via upload

---
+++ Classes/JAVASCRIPT/CLASS 1/app.js
@@ -0,0 +1,13 @@
+document.write(2+' AFFAN ')
+alert('THANK YOU FOR SUBMITTING THE FORM')
+//alert(2+2)
+//alert('this is first class')
+console.log('this is first class')
+var firstname ="Ahmed Affan Raza";
+document.write(firstname )
+var no1 =987
+var no2 =9
+var no3 =876
+document.write("<hr>")
+document.write(no1+no2*no3)
+document.write("<hr><table border=10><tr color=yellow><th>heading 1</th><th>heading 2</th></tr></table>")
